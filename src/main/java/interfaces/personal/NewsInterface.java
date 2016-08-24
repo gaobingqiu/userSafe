847ccaff9d3911231444867abeee08837a456f08
gbq <990924291@qq.com>
2016-08-24 18:15:28 +0800

    news

--- src/main/java/interfaces/personal/NewsInterface.java
+++ src/main/java/interfaces/personal/NewsInterface.java
@@ -1,56 +1,64 @@
-//package interfaces.personal;
-//
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.io.InputStreamReader;
-//import java.net.HttpURLConnection;
-//import java.net.URL;
-//
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-//@Controller
-//@RequestMapping("/NewsInterface")
-//public class NewsInterface {
-//
-//	@RequestMapping("/login")
-//	@ResponseBody
-//	public void login(HttpServletRequest request, HttpServletResponse response)
-//			throws ServletException, IOException {
-//		String httpUrl = "http://api.tianapi.com/wxnew/?key=您的key&num&rand";
-//		String jsonResult = request(httpUrl);
-//		System.out.println(jsonResult);
-//	}
-//
-//	public static String request(String httpUrl, String httpArg) {
-//	    BufferedReader reader = null;
-//	    String result = null;
-//	    StringBuffer sbf = new StringBuffer();
-//	    httpUrl = httpUrl + "?" + httpArg;
-//
-//	    try {
-//	        URL url = new URL(httpUrl);
-//	        HttpURLConnection connection = (HttpURLConnection) url
-//	                .openConnection();
-//	        connection.setRequestMethod("GET");
-//	        InputStream is = connection.getInputStream();
-//	        reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-//	        String strRead = null;
-//	        while ((strRead = reader.readLine()) != null) {
-//	            sbf.append(strRead);
-//	            sbf.append("\r\n");
-//	        }
-//	        reader.close();
-//	        result = sbf.toString();
-//	    } catch (Exception e) {
-//	        e.printStackTrace();
-//	    }
-//	    return result;
-//}
-//}
+package interfaces.personal;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URL;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.http.HttpRequest;
+
+@Controller
+@RequestMapping("/NewsInterface")
+public class NewsInterface {
+	String url;
+	// 请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
+	String param;
+	String httpUrl;
+	String httpArg = "num=10&page=1";
+
+	@RequestMapping("/getGlobalNews")
+	@ResponseBody
+	public void getGlobalNews(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		httpUrl = "http://apis.baidu.com/txapi/world/world";
+		String jsonResult = HttpRequest.request(httpUrl, httpArg);
+		PrintWriter out = response.getWriter();
+		out.print(jsonResult);
+		out.flush();
+		out.close();
+	}
+
+	@RequestMapping("/getPElNews")
+	@ResponseBody
+	public void getPElNews(HttpServletRequest request,
+			HttpServletResponse response)
+
+	throws ServletException, IOException {
+		httpUrl = "http://apis.baidu.com/txapi/world/world";
+		String jsonResult = HttpRequest.request(httpUrl, httpArg);
+		PrintWriter out = response.getWriter();
+		out.print(jsonResult);
+		out.flush();
+		out.close();
+	}
+
+	@RequestMapping("/getTeNews")
+	@ResponseBody
+	public void getTeNews(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		httpUrl = "http://apis.baidu.com/txapi/world/world";
+		String jsonResult = HttpRequest.request(httpUrl, httpArg);
+		PrintWriter out = response.getWriter();
+		out.print(jsonResult);
+		out.flush();
+		out.close();
+	}
+
+}
